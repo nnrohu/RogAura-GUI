40d3aaab736b7277e70ef736bac080ec3f7d3b76
Rohit Kumar <nnrohu@gmail.com>
2025-06-30 20:27:21 +0530

    Initial Commit

---
+++ RogAura/RogAuraApp.swift
@@ -0,0 +1,17 @@
+//
+//  RogAuraApp.swift
+//  RogAura
+//
+//  Created by Rohit on 30/06/25.
+//
+
+import SwiftUI
+
+@main
+struct RogAuraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
